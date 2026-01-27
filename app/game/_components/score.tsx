56921dfdd57c35f0d751042358f8c54dbbcbc277
Merk42 <mark_c@markecurtis.com>
2026-01-26 20:09:18 -0500

    switch bolded element

--- app/game/_components/score.tsx
+++ app/game/_components/score.tsx
@@ -9,16 +9,16 @@ export default function Score() {
     return (
         <div>
             <div className="grid grid-cols-[repeat(2,1fr)] w-full">
-                <div className={`${firsttoss === 'red' ? 'font-bold' : ''} bg-red-600 place-content-center aspect-[1] text-center text-white text-[10vw]`}>
+                <div className={`bg-red-600 place-content-center aspect-[1] text-center text-white text-[10vw]`}>
                 {score.red}
                 </div>
-                <div className={`${firsttoss === 'blue' ? 'font-bold' : ''} bg-blue-600 place-content-center aspect-[1] text-center text-white text-[10vw]`}>
+                <div className={`bg-blue-600 place-content-center aspect-[1] text-center text-white text-[10vw]`}>
                 {score.blue}
                 </div>
             </div>
             <div className="grid grid-cols-[repeat(2,1fr)] w-full">
-                <div className="text-center truncate text-lg">{redteam}</div>
-                <div className="text-center truncate text-lg">{blueteam}</div>
+                <div className={`${firsttoss === 'red' ? 'font-bold' : ''} text-center truncate text-lg`}>{redteam}</div>
+                <div className={`${firsttoss === 'blue' ? 'font-bold' : ''} text-center truncate text-lg`}>{blueteam}</div>
             </div>
         </div>
     )
