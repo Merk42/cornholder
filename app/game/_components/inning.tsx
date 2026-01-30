8612c5a635d9d2f7d54465430c81956d7c5b28b2
Merk42 <mark_c@markecurtis.com>
2026-01-29 21:26:35 -0500

    dark mode adjustments

--- app/game/_components/inning.tsx
+++ app/game/_components/inning.tsx
@@ -118,7 +118,7 @@ export default function Inning() {
 
     return (
         <div>
-            <div className="flex flex-wrap border-red-800 border-2 bg-red-100 p-2 my-4 shadow-xl/30">
+            <div className="flex flex-wrap border-red-800 border-2 bg-red-100 dark:bg-red-800 p-2 my-4 shadow-xl/30">
                 <h2 className="w-full text-2xl font-bold">
                     {redteam}
                 </h2>
@@ -128,7 +128,7 @@ export default function Inning() {
                         {numbers.map((number) => (
                             <span className='flex grow' key={number}>
                                 <input className="hidden peer" id={'onboard-red-' + number} type="radio" name="red-onboard" value={number} onChange={handleOnboardRedChange} checked={onBoardRedValue === number}/>
-                                <label htmlFor={'onboard-red-' + number} className={`border-red-800 bg-red-300 peer-checked:bg-red-800 cursor-pointer grow text-center border-2 p-2 rounded-md peer-checked:text-white`}>{number}</label>
+                                <label htmlFor={'onboard-red-' + number} className={`border-red-800 bg-red-300 peer-checked:bg-red-800 cursor-pointer grow text-center border-2 p-2 rounded-md text-black peer-checked:text-white`}>{number}</label>
                             </span>
                         ))}
                     </fieldset>
@@ -137,14 +137,14 @@ export default function Inning() {
                         {numbers.map((number) => (
                             <span className='flex grow' key={number}>
                                 <input className="hidden peer" id={'inhole-red-' + number} type="radio" name="red-inhole" value={number} onChange={handleInHoleRedChange} checked={inHoleRedValue === number}/>
-                                <label htmlFor={'inhole-red-' + number} className={`border-red-800 bg-red-300 peer-checked:bg-red-800 cursor-pointer grow text-center border-2 p-2 rounded-md peer-checked:text-white`}>{number}</label>
+                                <label htmlFor={'inhole-red-' + number} className={`border-red-800 bg-red-300 peer-checked:bg-red-800 cursor-pointer grow text-center border-2 p-2 rounded-md text-black peer-checked:text-white`}>{number}</label>
                             </span>
                         ))}
                     </fieldset>
                 </div>
-                <div className="flex-none w-[3ch] text-center self-center text-3xl text-red-800">{rTotal !== 99 ? rTotal : '--'}</div>
+                <div className="flex-none w-[3ch] text-center self-center text-3xl text-red-800 dark:text-red-100">{rTotal !== 99 ? rTotal : '--'}</div>
             </div>
-            <div className="flex flex-wrap border-blue-800 border-2 bg-blue-100 p-2 my-4 shadow-xl/30">
+            <div className="flex flex-wrap border-blue-800 border-2 bg-blue-100 dark:bg-blue-800 p-2 my-4 shadow-xl/30">
                 <h2 className="w-full text-2xl font-bold">
                     {blueteam}
                 </h2>
@@ -154,7 +154,7 @@ export default function Inning() {
                         {numbers.map((number) => (
                             <span className='flex grow' key={number}>
                                 <input className="hidden peer" id={'onboard-blue-' + number} type="radio" name="blue-onboard" value={number} onChange={handleOnboardBlueChange} checked={onBoardBlueValue === number}/>
-                                <label htmlFor={'onboard-blue-' + number} className={`border-blue-800 bg-blue-300 peer-checked:bg-blue-800 cursor-pointer grow text-center border-2 p-2 rounded-md peer-checked:text-white`}>{number}</label>
+                                <label htmlFor={'onboard-blue-' + number} className={`border-blue-800 bg-blue-300 peer-checked:bg-blue-800 cursor-pointer grow text-center border-2 p-2 rounded-md text-black peer-checked:text-white`}>{number}</label>
                             </span>
                         ))}
                     </fieldset>
@@ -163,12 +163,12 @@ export default function Inning() {
                         {numbers.map((number) => (
                             <span className='flex grow' key={number}>
                                 <input className="hidden peer" id={'inhole-blue-' + number} type="radio" name="blue-inhole" value={number} onChange={handleInHoleBlueChange} checked={inHoleBlueValue === number}/>
-                                <label htmlFor={'inhole-blue-' + number} className={`border-blue-800 bg-blue-300 peer-checked:bg-blue-800 cursor-pointer grow text-center border-2 p-2 rounded-md peer-checked:text-white`}>{number}</label>
+                                <label htmlFor={'inhole-blue-' + number} className={`border-blue-800 bg-blue-300 peer-checked:bg-blue-800 cursor-pointer grow text-center border-2 p-2 rounded-md text-black peer-checked:text-white`}>{number}</label>
                             </span>
                         ))}
                     </fieldset>
                 </div>
-                <div className="flex-none w-[3ch] text-center self-center text-3xl text-blue-800">{bTotal !== 99 ? bTotal : '--'}</div>
+                <div className="flex-none w-[3ch] text-center self-center text-3xl text-blue-800 dark:text-blue-100">{bTotal !== 99 ? bTotal : '--'}</div>
             </div>
             <button className={`${buttonColor} text-center w-full mt-2 p-4 text-white capitalize`} onClick={handleScoreUpdate} disabled={bTotal === 99 || rTotal === 99}>{buttonText}</button>
         </div>
