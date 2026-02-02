4ecee0d1e88155aef3b2c66fa333ed62a37f1bb9
Merk42 <mark_c@markecurtis.com>
2026-02-02 12:47:41 -0500

    similar name and color layout

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useCounterStore } from '../providers/counter-store-provider'
-import { DEFAULT_BUTTON, TEAM_SELECT, THEME } from '../const/style';
+import { DEFAULT_BUTTON, TEAM_SELECT, THEME, BAG_BORDER, BAG_BUTTON } from '../const/style';
 
 export default function Start() {
     const { setTeam1Name, setTeam2Name, setFirst, setTeam1Color, setTeam2Color } = useCounterStore(
@@ -89,33 +89,43 @@ export default function Start() {
     return (
         <form className='max-w-3xl px-4 mx-auto h-dvh place-content-center'>
             <h1 className='text-4xl my-4 text-center'>Set Teams</h1>
-            <div className='mt-2'>
-                <label htmlFor="team1name">Team 1 Name</label>
-                <select className={`${TEAM_SELECT['base']}${TEAM_SELECT[localTeam1Color]}`} id="team1name" value={localTeam1Name} onChange={handleTeam1NameChange}>
-                    {TEAMS.map((team) => (
-                        <option value={team} key={team}>{team}</option>
-                    ))}
-                </select>
-                <label htmlFor="team1color">Team 1 Color</label>
-                <select className={`${TEAM_SELECT['base']}${TEAM_SELECT[localTeam1Color]}`} id="team1color" value={localTeam1Color} onChange={handleTeam1ColorChange}>
-                    {THEMES.map((color) => (
-                        <option value={color} key={color}>{color}</option>
-                    ))}
-                </select>
+            <div className={`${BAG_BORDER['base']} ${BAG_BORDER[localTeam1Color]}`}>
+                <h2 className="w-full text-2xl font-bold">
+                    Team 1
+                </h2>
+                <div className="flex-1">                                
+                    <label htmlFor='team1name' className="capitalize block">name</label>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam1Color]} text-left w-full`} id="team1name" value={localTeam1Name} onChange={handleTeam1NameChange}>
+                        {TEAMS.map((team) => (
+                            <option value={team} key={team}>{team}</option>
+                        ))}
+                    </select>                               
+                    <label htmlFor='team1color' className="capitalize block">color</label>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam1Color]} text-left w-full`} id="team1color" value={localTeam1Color} onChange={handleTeam1ColorChange}>
+                        {THEMES.map((color) => (
+                            <option value={color} key={color}>{color}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
-            <div className='mt-2'>
-                <label htmlFor="team2name">Team 2 Name</label>
-                <select className={`${TEAM_SELECT['base']}${TEAM_SELECT[localTeam2Color]}`} id="team2name" value={localTeam2Name} onChange={handleTeam2NameChange}>
-                    {TEAMS.map((team) => (
-                        <option value={team} key={team}>{team}</option>
-                    ))}
-                </select>
-                <label htmlFor="team1color">Team 2 Color</label>
-                <select className={`${TEAM_SELECT['base']}${TEAM_SELECT[localTeam2Color]}`} id="team1color" value={localTeam2Color} onChange={handleTeam2ColorChange}>
-                    {THEMES.map((color) => (
-                        <option value={color} key={color}>{color}</option>
-                    ))}
-                </select>                
+            <div className={`${BAG_BORDER['base']} ${BAG_BORDER[localTeam2Color]}`}>
+                <h2 className="w-full text-2xl font-bold">
+                    Team 2
+                </h2>
+                <div className="flex-1">                                
+                    <label htmlFor='team2name' className="capitalize block">name</label>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam2Color]} text-left w-full`} id="team2name" value={localTeam2Name} onChange={handleTeam2NameChange}>
+                        {TEAMS.map((team) => (
+                            <option value={team} key={team}>{team}</option>
+                        ))}
+                    </select>                               
+                    <label htmlFor='team2color' className="capitalize block">color</label>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam2Color]} text-left w-full`} id="team2color" value={localTeam2Color} onChange={handleTeam2ColorChange}>
+                        {THEMES.map((color) => (
+                            <option value={color} key={color}>{color}</option>
+                        ))}
+                    </select>
+                </div>
             </div>
             <div className='text-center mt-4'>
                 <button type='button' className={DEFAULT_BUTTON} onClick={handleGameStart} disabled={localTeam1Name === '--- SELECT ---' || localTeam2Name === '--- SELECT ---'}>
