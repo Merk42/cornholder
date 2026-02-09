4814cfa6d76709be094c416436a518682c13adb0
Merk42 <mark_c@markecurtis.com>
2026-02-09 15:56:26 -0500

    data reorganization and schedule

---
+++ app/const/data.ts
@@ -0,0 +1,158 @@
+import type { THEME } from "./style"
+type TEAMSS = {
+    id:number;
+    name:string;
+    theme?:THEME
+}[]
+
+export const TEAMS:TEAMSS = [
+    {
+        id:1,
+        name: "B Team"
+    },
+    {
+        id:2,
+        name: "Bombs Away",
+        theme: 'rose'
+    },
+    {
+        id:3,
+        name: "Bumble&Fumble",
+        theme: 'purple'
+    },
+    {
+        id:4,
+        name: "Business Casual"
+    },
+    {
+        id:5,
+        name: "Came Out Slingin'",
+        theme:'teal'
+    },
+    {
+        id:6,
+        name: "Exciting Whites",
+        theme: 'indigo'
+    },
+    {
+        id:7,
+        name: "Floppy Bags",
+        theme: 'pink'
+    },
+    {
+        id:8,
+        name: "Just The Tip"
+    },
+    {
+        id:9,
+        name: "Noga",
+        theme: 'blue'
+    },
+    {
+        id:10,
+        name: "ROOT of all Problems*",
+        theme: 'amber'
+    },
+    {
+        id:11,
+        name: "Shitz and Giggles (DRI-FIT)",
+        theme: 'emerald'
+    },
+    {
+        id:12,
+        name: "Shuck It"
+    },
+    {
+        id:13,
+        name: "Team 390",
+        theme: 'green'
+    },
+    {
+        id:14,
+        name: "Team Mahoney (RE-USE)",
+        theme: 'orange'
+    },
+    {
+        id:15,
+        name: "The Riff Raff"
+    },
+    {
+        id:16,
+        name: "Tooth Girls 1 Bag",
+        theme: 'sky'
+    },
+    {
+        id:17,
+        name: "UniCORNs*",
+        theme: 'lime'
+    },
+]
+
+export const GAMES = [
+    {
+        "date":"2026-02-12",
+        "times":[
+            {
+                "time":"1830",
+                "games":[
+                    {
+                        "board":1,
+                        "team1":6,
+                        "team2":7
+                    },
+                    {
+                        "board":2,
+                        "team1":12,
+                        "team2":3
+                    },
+                    {
+                        "board":3,
+                        "team1":1,
+                        "team2":13
+                    }
+                ]
+            },
+            {
+                "time":"1915",
+                "games":[
+                    {
+                        "board":1,
+                        "team1":4,
+                        "team2":14
+                    },
+                    {
+                        "board":2,
+                        "team1":8,
+                        "team2":15
+                    },
+                    {
+                        "board":3,
+                        "team1":12,
+                        "team2":2
+                    }
+                ]
+            },
+            {
+                "time":"2000",
+                "games":[
+                    {
+                        "board":1,
+                        "team1":8,
+                        "team2":5
+                    },
+                    {
+                        "board":2,
+                        "team1":11,
+                        "team2":12
+                    },
+                    {
+                        "board":3,
+                        "team1":17,
+                        "team2":4
+                    }
+                ]
+            }
+
+        ]
+    }
+]
--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,35 +1,70 @@
 'use client'; // Required for client-side hooks in the App Router
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useCounterStore } from '../providers/counter-store-provider'
 import { DEFAULT_BUTTON, THEME, BAG_BORDER, BAG_BUTTON } from '../const/style';
+
+import { TEAMS, GAMES } from '../const/data';
+
+function Schedule({onEmitData}) {
+
+    function getNextThursday(date = new Date()) {
+        // Thursday is represented by 4
+        const THURSDAY = 4;
+        const currentDay = date.getDay();
+        // Calculate the number of days until the next Thursday
+        // If today is Thursday, it returns the Thursday in the current week. 
+        // If you need the next Thursday (i.e. next week), you can adjust the logic slightly.
+        const daysToAdd = (THURSDAY - currentDay + 7) % 7; 
+        
+        // Create a new Date object to avoid modifying the original date
+        const nextThursday = new Date(date);
+        // Set the date by adding the calculated days
+        nextThursday.setDate(date.getDate() + daysToAdd);
+        
+        return nextThursday.toISOString().split("T")[0];
+    }
+
+    const ISOTOUS = (ISO:string) => {
+        const MIN = ISO[2] + ISO[3];
+        const HR = Number(ISO[0] + ISO[1])
+        const AMPM = HR >= 12 ? 'PM' : 'AM';
+        return `${HR % 12}:${MIN} ${AMPM}`
+    }
+
+    const upcoming = useMemo(() => {
+        const D =  getNextThursday();
+        return GAMES.find(game => game.date === D) || {date:"", times:[]}
+    }, [])
+
+    return (
+        <div>
+            {upcoming.times.map(games => (
+                <div key={games.time} className=" border-2 p-2 mb-4">
+                    <h1 className='text-3xl'>{ISOTOUS(games.time)}</h1>
+                    {games.games.map(game => (
+                        <div key={game.board} className='flex mt-2'>
+                            <div className='flex-1'>
+                                 <h2 className='text-2xl'>Board {game.board}</h2>
+                                <p>{TEAMS.find(team => team.id === game.team1)?.name || ''} vs {TEAMS.find(team => team.id === game.team2)?.name || ''}</p>
+                            </div>
+                            <div className='flex-0'>
+                                <button type='button' onClick={() => onEmitData([game.team1, game.team2])}>start</button>
+                            </div>      
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+}
 
 export default function Start() {
     const { setTeam1Name, setTeam2Name, setFirst, setTeam1Color, setTeam2Color } = useCounterStore(
         (state) => state,
     )
 
-    const TEAMS = [
-        "--- SELECT ---",
-        "B Team",
-        "Bombs Away",
-        "Bumble&Fumble",
-        "Business Casual",
-        "Came Out Slingin'",
-        "Exciting Whites",
-        "Floppy Bags",
-        "Noga",
-        "ROOT of all Problems*",
-        "Shitz and Giggles (DRI-FIT)",
-        "Shuck It",
-        "Team 390",
-        "Team Mahoney (RE-USE)",
-        "The Riff Raff",
-        "Tooth Girls 1 Bag",
-        "UniCORNs*"
-    ]
-
-    const THEMES = [
+    const THEMES:THEME[] = [
         'red',
         'orange',
         'amber',
@@ -57,25 +92,38 @@ export default function Start() {
     };
 
     const handleGameStart = () => {
-        setTeam1Name(localTeam1Name);
-        setTeam2Name(localTeam2Name);
+        const TEAM1NAME = TEAMS.find(team => team.id === localTeam1ID)?.name || '';
+        const TEAM2NAME = TEAMS.find(team => team.id === localTeam2ID)?.name || '';
+        setTeam1Name(TEAM1NAME);
+        setTeam2Name(TEAM2NAME);
         setTeam1Color(localTeam1Color);
         setTeam2Color(localTeam2Color);
         coinFlip();
         router.push('/game');
     }
 
-    const [localTeam1Name, setLocalTeam1Name] = useState('--- SELECT ---');
-    const [localTeam2Name, setLocalTeam2Name] = useState('--- SELECT ---');
+    const handleChildData = (dataFromChild:number[]) => {
+        const TEAM1 = TEAMS.find(team => team.id === dataFromChild[0]) || {id: dataFromChild[0], name:"", theme:'base'};
+        const TEAM2 = TEAMS.find(team => team.id === dataFromChild[1]) || {id: dataFromChild[1], name:"", theme:'base'};
+        setTeam1Name(TEAM1.name);
+        setTeam2Name(TEAM2.name);
+        setTeam1Color(TEAM1.theme || 'base');
+        setTeam2Color(TEAM2.theme || 'base');
+        coinFlip();
+        router.push('/game');
+    };
+
+    const [localTeam1ID, setLocalTeam1ID] = useState(0);
+    const [localTeam2ID, setLocalTeam2ID] = useState(0);
     const [localTeam1Color, setLocalTeam1Color] = useState<THEME>('red');
     const [localTeam2Color, setLocalTeam2Color] = useState<THEME>('blue');
 
     const handleTeam1NameChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
-        setLocalTeam1Name(event.target.value);
+        setLocalTeam1ID(Number(event.target.value));
     }
 
     const handleTeam2NameChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
-        setLocalTeam2Name(event.target.value);
+        setLocalTeam2ID(Number(event.target.value));
     }
 
     const handleTeam1ColorChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
@@ -88,6 +136,7 @@ export default function Start() {
 
     return (
         <form className='max-w-3xl px-4 mx-auto h-dvh place-content-center'>
+            <Schedule onEmitData={handleChildData}/>
             <h1 className='text-4xl my-4 text-center'>Set Teams</h1>
             <div className={`${BAG_BORDER['base']} ${BAG_BORDER[localTeam1Color]}`}>
                 <h2 className="w-full text-2xl font-bold">
@@ -95,9 +144,10 @@ export default function Start() {
                 </h2>
                 <div className="flex-1">                                
                     <label htmlFor='team1name' className="capitalize block">name</label>
-                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam1Color]} text-left w-full`} id="team1name" value={localTeam1Name} onChange={handleTeam1NameChange}>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam1Color]} text-left w-full`} id="team1name" value={localTeam1ID} onChange={handleTeam1NameChange}>
+                        <option value={0}>--- SELECT ---</option>
                         {TEAMS.map((team) => (
-                            <option value={team} key={team}>{team}</option>
+                            <option value={team.id} key={team.id}>{team.name}</option>
                         ))}
                     </select>                               
                     <label htmlFor='team1color' className="capitalize block">color</label>
@@ -114,9 +164,10 @@ export default function Start() {
                 </h2>
                 <div className="flex-1">                                
                     <label htmlFor='team2name' className="capitalize block">name</label>
-                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam2Color]} text-left w-full`} id="team2name" value={localTeam2Name} onChange={handleTeam2NameChange}>
+                    <select className={`${BAG_BUTTON['base']} ${BAG_BUTTON[localTeam2Color]} text-left w-full`} id="team2name" value={localTeam2ID} onChange={handleTeam2NameChange}>
+                        <option value={0}>--- SELECT ---</option>
                         {TEAMS.map((team) => (
-                            <option value={team} key={team}>{team}</option>
+                            <option value={team.id} key={team.id}>{team.name}</option>
                         ))}
                     </select>                               
                     <label htmlFor='team2color' className="capitalize block">color</label>
@@ -128,7 +179,7 @@ export default function Start() {
                 </div>
             </div>
             <div className='text-center mt-4'>
-                <button type='button' className={DEFAULT_BUTTON} onClick={handleGameStart} disabled={localTeam1Name === '--- SELECT ---' || localTeam2Name === '--- SELECT ---'}>
+                <button type='button' className={DEFAULT_BUTTON} onClick={handleGameStart} disabled={localTeam1ID === 0 || localTeam2ID === 0}>
                     start game
                 </button>
             </div>
