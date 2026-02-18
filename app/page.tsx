c482cc6ee24e11080a2c8299327c61ea86678837
Merk42 <mark_c@markecurtis.com>
2026-02-18 09:11:14 -0500

    championship link

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <div className='max-w-3xl px-4 mx-auto text-center h-dvh place-content-center'>
       <h1 className='text-4xl my-4'>Corn Hole!</h1>
       <Link className={DEFAULT_BUTTON} href="/start">New Game</Link>
+      <hr  className='my-4'></hr>
+      <Link className={DEFAULT_BUTTON} href="/championship">Championship</Link>
     </div>
   );
 }
