7ca7b84a4368374874beaf5322f0db998bf2114a
Merk42 <mark_c@markecurtis.com>
2026-02-19 10:59:27 -0500

    reworking championship bracket

--- app/championship/page.tsx
+++ app/championship/page.tsx
@@ -6,108 +6,78 @@ import { BAG_BUTTON } from "../const/style";
 export default function Championship() {
 
     interface R {
-        board:number;
+        round?:number;
+        board?:number;
         team1:string;
         team2:string
     }
 
-
-
     const pairedup = useMemo(() => {
-
-        const BR:R[][] = [
-            [
-                {
-                    board:1,
-                    team1:'1',
-                    team2:'2'
-                },
-                {
-                    board:2,
-                    team1:'3',
-                    team2:'4'
-                },
-                {
-                    board:3,
-                    team1:'5',
-                    team2:'6'
-                },
-                {
-                    board:1,
-                    team1:'7',
-                    team2:'8'
-                },
-                {
-                    board:2,
-                    team1:'9',
-                    team2:'10'
-                },
-                {
-                    board:3,
-                    team1:'11',
-                    team2:'12'
-                },
-                {
-                    board:1,
-                    team1:'13',
-                    team2:'14'
-                },
-                {
-                    board:2,
-                    team1:'15',
-                    team2:'16'
-                }                                    
-            ],
-            [
-                {
-                    board:1,
-                    team1:'a',
-                    team2:'a'
-                },
-                {
-                    board:2,
-                    team1:'b',
-                    team2:'b'
-                },
-                {
-                    board:3,
-                    team1:'c',
-                    team2:'c'
-                },
-                {
-                    board:1,
-                    team1:'d',
-                    team2:'d'
-                }
-            ],
-            [
-                {
-                    board:1,
-                    team1:'e',
-                    team2:'e'
-                },
-                {
-                    board:2,
-                    team1:'f',
-                    team2:'f'
-                }
-            ],
-            [
-                {
-                    board:1,
-                    team1:'g',
-                    team2:'g'
-                }
-            ]
+        const DB = [
+            {
+                round:1,
+                team1:'12',
+                team2:'10'
+            },
+            {
+                round:1,
+                team1:'7',
+                team2:'6'
+            },
+            {
+                round:1,
+                team1:'13',
+                team2:'15'
+            },
+            {
+                round:1,
+                team1:'7',
+                team2:'2'
+            },
+            {
+                round:1,
+                team1:'8',
+                team2:'3'
+            },
+            {
+                round:1,
+                team1:'14',
+                team2:'17'
+            },
+            {
+                round:1,
+                team1:'5',
+                team2:'16'
+            },
+            {
+                round:1,
+                team1:'11',
+                team2:'9'
+            }
         ]
-
-        const pairArray = (arr:R[]) => {
-        const paired = [];
-        for (let i = 0; i < arr.length; i += 2) {
-            // Slice two elements to create a pair
-            paired.push(arr.slice(i, i + 2));
+        const groupedByObject = Object.groupBy(DB, (product) => {
+            return product.round;
+        });
+        const ROUNDS = 4;
+        const BR:R[][] = [];
+        let placeholder = 65;
+        for (let i = 1; i <= ROUNDS; i++) {
+            if (groupedByObject[i]?.length) {
+                BR.push(groupedByObject[i] || [])
+            } else {
+                const EXPONENT = ROUNDS - i;
+                const ARRLENGTH = 2**EXPONENT;
+                BR.push(new Array(ARRLENGTH).fill({team1:String.fromCharCode(placeholder),team2:String.fromCharCode(placeholder)}))
+                placeholder++;
+            }
         }
-        return paired;
+        const pairArray = (arr:R[]) => {
+            const paired = [];
+            for (let i = 0; i < arr.length; i += 2) {
+                // Slice two elements to create a pair
+                paired.push(arr.slice(i, i + 2));
+            }
+            return paired;
         };
 
 
@@ -118,7 +88,7 @@ export default function Championship() {
     return (
         <div className={`grid grid-cols-4 gap-3`}>
             {pairedup.map((column, i) => (
-                <div key={i} className="h-dvh flex flex-col justify-around pairing-col">
+                <div key={i} className="gap-8 flex flex-col justify-around pairing-col">
                     {column.map((pair, index) => (
                         // A unique key is important for React to efficiently update the DOM
                         <div key={index} className="four">
