9d658e95a2e773146c29e103c611523df2491ec2
Merk42 <mark_c@markecurtis.com>
2026-02-20 19:05:48 -0500

    footer gap

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen gap-3`}
       >
         <CounterStoreProvider>
           <main className="grow">{children}</main>
-          <footer className="sticky bottom-0" >
+          <footer className="sticky bottom-0 bg-background" >
             <nav className="flex justify-between max-w-3xl px-4 mx-auto">
               <Link className={`${DEFAULT_BUTTON} whitespace-nowrap`} href="/start">New Game</Link>
               <Link className={`${DEFAULT_BUTTON} whitespace-nowrap`} href="/standings">Standings</Link>
