b3ddd2893b80e198c599fa6075e7fbe92ff1155c
Merk42 <mark_c@markecurtis.com>
2026-03-05 10:49:16 -0500

    remove flex

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen gap-3`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased gap-3`}
       >
         <CornholeStoreProvider>
           {children}
